67c32b11391be1f77da5d97c67525cea821fd2d0
guidaohuangxing <guidao12@163.com>
2021-10-09 17:05:38 -0400

    bg

--- Assets/Scripts/BackGroundManager.cs
+++ Assets/Scripts/BackGroundManager.cs
@@ -5,46 +5,41 @@ using Tobii.Gaming;
 
 public class BackGroundManager : MonoBehaviour
 {
+    public List<RectTransform> BG;
 
-    public Transform BG1;
-    public Transform BG2;
-    public Transform BG3;
-    public Transform BG4;
-
-    public float speed1 = 1;
-    public float speed2 = 2;
-    public float speed3 = 3;
-    public float speed4 = 4;
-
+    public List<float> speed;
+    private Vector3 VSpeed;
+    GazePoint _lastGazePoint = GazePoint.Invalid;
     private GazePoint gazePoint;
     public Canvas canvas;
 
     private void FixedUpdate()
     {
         Parallax();
+    
     }
 
     private void Parallax()
     {
         gazePoint = TobiiAPI.GetGazePoint();
-        Vector3 BG1TargetPos = ChangeGazePointToTarget(gazePoint, speed1);
-        Vector3 BG2TargetPos = ChangeGazePointToTarget(gazePoint, speed2);
-        Vector3 BG3TargetPos = ChangeGazePointToTarget(gazePoint, speed3);
-        Vector3 BG4TargetPos = ChangeGazePointToTarget(gazePoint, speed4);
-        BG1.position = Vector3.Lerp(BG1.position, BG1TargetPos, 0.1f);
-        BG2.position = Vector3.Lerp(BG2.position, BG2TargetPos, 0.1f);
-        BG3.position = Vector3.Lerp(BG3.position, BG3TargetPos, 0.1f);
-        BG4.position = Vector3.Lerp(BG4.position, BG4TargetPos, 0.1f);
+        for(int i = 0; i < BG.Count; i++)
+        {
+            
+            Vector3 target = ChangeGazePointToTarget(gazePoint, speed[i]);
+            BG[i].localPosition = Vector3.Lerp(BG[i].localPosition, target, 0.5f);
+        }
+        _lastGazePoint = gazePoint;
 
 
     }
 
     private Vector3 ChangeGazePointToTarget(GazePoint gazePoint, float speed)
     {
-        Vector2 viewPort = gazePoint.Viewport - new Vector2(-.5f, -.5f);
-        Vector2 eyePoint = viewPort * new Vector2(canvas.renderingDisplaySize.x, canvas.renderingDisplaySize.y);
+        Vector2 viewPort = gazePoint.Viewport - new Vector2(.5f, .5f);
+        Vector2 eyePoint = viewPort ;
         Vector3 TargetPos = new Vector3(Mathf.Atan(eyePoint.x) * speed, 0, 0);
         return TargetPos;
     }
+
 
 }
