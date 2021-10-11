498d59e22e49357bc02feea46d59ca3833e20c7a
kirin111697 <kirinonfire6373@gmail.com>
2021-10-10 21:10:59 -0400

    Update SoundManager.cs

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class SoundManager : MonoBehaviour
 {
     public static SoundManager Instance { get => _instance; }
     static SoundManager _instance;
+    [SerializeField] AudioClip[] clips;
+    AudioSource _audiosource;
 
     // Start is called before the first frame update
     private void Awake()
@@ -22,12 +25,20 @@ public class SoundManager : MonoBehaviour
 
     void Start()
     {
-        
+        _audiosource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void PlaySound(int index)
+    {
+        if (!_audiosource.isPlaying)
+        {
+            _audiosource.PlayOneShot(clips[index]);
+        }
     }
 }
